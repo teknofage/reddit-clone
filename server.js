feadd6c95c23c27688b4d404504168d5a96f43af
teknofage <teknofage@gmail.com>
2020-06-11 20:04:00 -0700

    rendering basic templates

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const port = 3000
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
