5069e2df5cf05b555a86fb2a1258692346ed00a1
teknofage <teknofage@gmail.com>
2020-06-15 23:22:55 -0700

    top of page 6, comments added but post route not working properly

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const expressValidator = require('express-validator');
 const app = express();
 const port = 3000
 
-// Set db
-require('./data/reddit-db');
-
 // Use Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,14 +17,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 require('./data/reddit-db');
 require('./controllers/posts.js')(app);
+require('./controllers/comments.js')(app);
 
-// app.get('/', (req, res) => {
-//     res.render('home')
-// });
-// app.get('/posts/new', (req, res) => {
-// 	res.render('posts-new')
-
-// });
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
 module.exports = app;
